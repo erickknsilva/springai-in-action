30a252cce4f4c8e4694053164964543da94b9ead
erickknsilva <erick.nsilva100@gmail.com>
2026-04-26 04:06:42 -0300

    Implementando RAG para procurar regras de um jogo especifico, que não existe no modelo de LLM gpt.4.0-mini.

--- src/main/java/com/example/boardgamebuddy/service/GameRuleService.java
+++ src/main/java/com/example/boardgamebuddy/service/GameRuleService.java
@@ -2,30 +2,66 @@ package com.example.boardgamebuddy.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.vectorstore.filter.FilterExpressionBuilder;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.stream.Collectors;
 
 @Service
 public class GameRuleService {
 
-    private static final Logger logger = LoggerFactory.getLogger(GameRuleService.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(GameRuleService.class);
+    private final VectorStore vectorStore;
 
-    public String getRulesForGame(String gameName){
 
-        try{
-            var fileName = String.format("classpath:gameRules/%s.txt",
-                    gameName.toLowerCase().replace(" ", "_"));
-            return new DefaultResourceLoader()
-                    .getResource(fileName)
-                    .getContentAsString(Charset.defaultCharset());
-        }
-        catch (IOException ex){
-            logger.info("No rules found for game: " + gameName);
-            return "";
-        }
+    public GameRuleService(VectorStore vectorStore) {
+        this.vectorStore = vectorStore;
     }
+
+
+    public String getRulesForGame(String gameName, String question){
+
+        var searchRequest = SearchRequest.builder()
+                .query(question)
+                .similarityThreshold(0.5)
+//                .topK(6)
+                .filterExpression(
+                        new FilterExpressionBuilder()
+                                .eq("gameTitle", normalizeGameTitle(gameName)).build())
+                .build();
+
+        System.err.println("Search request: " + searchRequest);
+        LOGGER.info("Search request: {}", searchRequest);
+
+        var similarDocs = vectorStore.similaritySearch(searchRequest);
+
+        if(similarDocs.isEmpty()){
+            LOGGER.error("No similar documents found");
+            return "The rules for " + gameName  + " are not available";
+        }
+
+        return  similarDocs.stream()
+                .map(Document::getText)
+                .collect(Collectors.joining(System.lineSeparator()));
+
+
+//            var fileName = String.format("classpath:gameRules/%s.txt",
+//                    gameName.toLowerCase().replace(" ", "_"));
+//
+//            return new DefaultResourceLoader()
+//                    .getResource(fileName)
+//                    .getContentAsString(Charset.defaultCharset());
+    }
+
+    private String normalizeGameTitle(String gameTitle) {
+        return gameTitle.toLowerCase().replace(" ", "_");
+    }
+
 
 }
